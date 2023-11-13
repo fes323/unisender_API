7d7044e856d697651a9170a012e17d05f1866d96
Legeyda <maksim.legeyda@gefera.ru>
2023-11-13 13:39:41 +0300

    add comments

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import pandas as pd
 # Замените API_KEY на ваш собственный
 API_KEY = ''
 
+'''
+Получение всех списков для сопостовления с ID
+'''
 def get_lists():
     url = f'https://api.unisender.com/ru/api/getLists?format=json&api_key={API_KEY}'
     response = requests.get(url)
@@ -15,7 +18,9 @@ def get_lists():
 
     return list_name_dict
 
-
+'''
+Получение рассылочных компаний
+'''
 def get_campaigns(from_time, to_time):
     url = f'https://api.unisender.com/ru/api/getCampaigns?format=json&api_key={API_KEY}&from={from_time}&to={to_time}'
     response = requests.get(url)
@@ -23,7 +28,9 @@ def get_campaigns(from_time, to_time):
     data_shortened = [{k: v for k, v in i.items() if k in ["id","sender_name", "subject", "sender_email", "list_id"]} for i in data['result']]
     return data_shortened
 
-
+'''
+Получение статистики по рассылочным компаниям.
+'''
 def get_campaign_stats(campaign, list_dict):
     url = f'https://api.unisender.com/ru/api/getCampaignCommonStats?format=json&api_key={API_KEY}&campaign_id={campaign["id"]}'
     response = requests.get(url)
@@ -34,19 +41,21 @@ def get_campaign_stats(campaign, list_dict):
     data['sender_name'] = campaign['sender_name']
     data['subject'] = campaign['subject']
     data['sender_email'] = campaign['sender_email']
-    # add list title to the stats
     data['list_name'] = list_dict.get(campaign['list_id'], 'Произошла ошибка при получении имени списка')
 
     return data
 
-
+'''
+Сохранение в XLSX документ используя Pandas.
+Последовательность колонок можно настроить.
+'''
 def save_to_excel(data, filename):
     df = pd.DataFrame(data)
     df = df[['sender_name', 'subject', 'sender_email', 'list_name', 'total', 'sent', 'delivered', 'read_all', 'clicked_all', 'unsubscribed', 'spammed', 'read_unique', 'clicked_unique', 'spam']]
     df.to_excel(filename, index=False)
 
 if __name__ == '__main__':
-    #Введите диапазон дат отправки
+    #Введите диапазон дат отправки YYYY-MM-DD hh:mm:ss
     from_time = '2023-10-01 00:00:01'
     to_time = '2023-10-13 23:59:59'
 
@@ -54,6 +63,7 @@ if __name__ == '__main__':
     campaigns = get_campaigns(from_time, to_time)
     campaign_stats = []
 
+    #Основной цикл
     for campaign in campaigns:
         try:
             stats = get_campaign_stats(campaign, list_dict)
@@ -61,6 +71,7 @@ if __name__ == '__main__':
         except requests.HTTPError as err:
             print(f"Error occurred while fetching campaign {campaign['id']} statistics: {err}")
 
+    #Наименование файла
     filename = 'unisender_stats.xlsx'
     save_to_excel(campaign_stats, filename)
     print(f"Статистика сохранена в файле: {filename}")
